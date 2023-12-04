94946317934fc569685c44cf52a704912bd0d297
adrianomisina <adriano.misina@gmail.com>
2023-12-04 11:03:06 -0300

    complete sign up page ui

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,12 +4,12 @@ import {FaSearch} from 'react-icons/fa'
 
 const Header = () => {
   return (
-     <header className='bg-slate-200 shadow-md'>
+     <header className='bg-slate-700 shadow-md'>
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
         <Link to='/'>
           <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-            <span className='text-slate-500'>Adriano</span>
-            <span className='text-slate-700'>Estate</span>
+            <span className='text-slate-100'>Adriano</span>
+            <span className='text-slate-300'>Estate</span>
           </h1>
         </Link>
         <form
@@ -21,22 +21,22 @@ const Header = () => {
             className='bg-transparent focus:outline-none w-24 sm:w-64'
           />
           <button>
-            <FaSearch className='text-slate-600' />
+            <FaSearch className='text-slate-100' />
           </button>
         </form>
         <ul className='flex gap-4'>
           <Link to='/'>
-            <li className='hidden sm:inline text-slate-700 hover:underline'>
+            <li className='hidden sm:inline text-slate-300 hover:underline'>
               Home
             </li>
           </Link>
           <Link to='/about'>
-            <li className='hidden sm:inline text-slate-700 hover:underline'>
+            <li className='hidden sm:inline text-slate-300 hover:underline'>
               About
             </li>
           </Link>
           <Link to='/sign-in'>
-            <li className=' text-slate-700 hover:underline'> Sign in</li>
+            <li className=' text-slate-300 hover:underline'> Sign in</li>
           </Link>
         </ul>
       </div>
